c4e371f78954401fb27052f8115980e8fd7a3975
Jonas Gabriel <jonas@pop-os.localdomain>
2020-06-29 19:12:52 -0300

    :tada: initial commit

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+
+app.use(routes);
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello World' });
+});
+
+app.listen(1337, () => {
+  console.log('Server started on port 1337');
+});
