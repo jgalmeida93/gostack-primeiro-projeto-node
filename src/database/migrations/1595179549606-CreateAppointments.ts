7d35e19f6475a0d61b7760a867e0f5d5fe202d9f
Jonas Gabriel <jgalmeida1993@gmail.com>
2020-09-10 18:55:55 -0300

    :ambulance: (quickfix) adding uuid generator to id column

--- src/database/migrations/1595179549606-CreateAppointments.ts
+++ src/database/migrations/1595179549606-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1595179549606
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
